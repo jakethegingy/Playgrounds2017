b33c73684e106be123bffa4076c8a74215537980
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-09-25 07:56:24 -0600

    Initial commit

---
+++ throw me away.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myName = "Jake Hammond"
+print(myName)
+var sample = "Gingy"
+print(sample)
+var sample = "Jingy"
+print(sample)
