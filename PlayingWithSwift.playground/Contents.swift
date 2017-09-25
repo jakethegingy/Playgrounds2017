0ebb5e6f6a2c87f2a9af7312cbd022d4d33d05be
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-09-25 09:34:55 -0600

    made multiple variables change type and then combined them in string form and int form.

--- PlayingWithSwift.playground/Contents.swift
+++ PlayingWithSwift.playground/Contents.swift
@@ -1,5 +1,36 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-var str = "Hello, playground"
+var ramen :String
+//Explicit type declaration with no value
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//Implicit type declaration with a value to assign type.
+var myAge :Int = 18
+print(myAge)
+//read colin as "of type"
+myAge = myAge / 3
+myAge = myAge * 3
+var someValue :Double = 3.5485 * 52
+someValue = someValue * 254.2
+var isReal :Bool
+isReal = false
+isReal = true
+var myName :String
+myName = "Toprameniqua"
+print(myName)
+myName = soup
+print(myName)
+print(someValue)
+someValue = someValue * 0
+var newValue = Int (someValue)
+print (newValue)
+newValue = newValue + myAge
+print (newValue)
+var textLine1 = "My age is now "
+var textLine2 = "!"
+var newValueText = String (newValue)
+var combinedTextLine = textLine1 + newValueText + textLine2
+print (combinedTextLine)
