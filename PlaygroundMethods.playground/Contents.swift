90ea908e5976a93a34f28181cdf2be8c1838a129
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-09-27 09:13:01 -0600

    did som more stuff

--- PlaygroundMethods.playground/Contents.swift
+++ PlaygroundMethods.playground/Contents.swift
@@ -18,3 +18,39 @@ public func evenLessSimpleMethod(count :Int, bandName :String, date :String) -> 
     print(info)
 }
 evenLessSimpleMethod(count: 4, bandName: "They Might Be Giants", date: "June, 1992")
+
+public func calculateDays(first firstDay :Int, secondDay :Int) -> Int
+{
+    let answer = secondDay - firstDay
+    
+    return answer
+}
+calculateDays(first: 4, secondDay:5)
+public func name(friendName homeName :String) -> Void
+{
+    let info = "At home my name is " + homeName
+    print(info)
+}
+name(friendName: "Gingy")
+
+var guess = 453
+if (guess > 500)
+{
+    print("wahoo!")
+}
+else if(guess ==  65)
+{
+    print("not likely")
+}
+while (guess > 0)
+{
+    print("🌕🌖🌗🌘🌑🌒🌓🌔🌕")
+    print("🌖🌗🌘🌑🌒🌓🌔🌕🌖")
+    print("🌗🌘🌑🌒🌓🌔🌕🌖🌗")
+    print("🌘🌑🌒🌓🌔🌕🌖🌗🌘")
+    print("🌑🌒🌓🌔🌕🌖🌗🌘🌑")
+    print("🌒🌓🌔🌕🌖🌗🌘🌑🌒")
+    print("🌓🌔🌕🌖🌗🌘🌑🌒🌓")
+    print("🌔🌕🌖🌗🌘🌑🌒🌓🌔")
+    guess -= 1
+}
